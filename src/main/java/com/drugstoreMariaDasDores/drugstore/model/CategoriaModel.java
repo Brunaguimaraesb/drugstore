7075d816108dcc38cf0d42a8f4111c34b5352ead
RenanUmV <renan.uchoa.m@gmail.com>
2021-11-08 13:56:54 -0300

    CREATE PROJECT

---
+++ src/main/java/com/drugstoreMariaDasDores/drugstore/model/CategoriaModel.java
@@ -0,0 +1,10 @@
+package com.drugstoreMariaDasDores.drugstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaModel {
+	
+}
---
+++ src/test/java/com/drugstoreMariaDasDores/drugstore/DrugstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drugstoreMariaDasDores.drugstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
