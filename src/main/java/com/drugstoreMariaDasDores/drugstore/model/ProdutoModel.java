189508b0a68d91f60583481fd6a6e06a572cb017
brunaguimaraesb <brunaguimaraes.santos@hotmail.com>
2021-11-08 15:06:54 -0300

    CREATE ProdutoCategoria

---
+++ src/main/java/com/drugstoreMariaDasDores/drugstore/model/ProdutoModel.java
@@ -0,0 +1,50 @@
+package com.drugstoreMariaDasDores.drugstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class ProdutoModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idProduto;
+
+	@NotBlank	
+	@Size(min = 5, max = 30)
+	private String nome;
+
+	@NotBlank	
+	private Double preco;
+
+	public long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
