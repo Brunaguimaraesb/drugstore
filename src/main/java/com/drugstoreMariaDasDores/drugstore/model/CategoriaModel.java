05c4c4ddb4dd53560f5c9273bc17bd19b81c3317
RenanUmV <renan.uchoa.m@gmail.com>
2021-11-08 14:11:15 -0300

    @RenanUmV [task: CREATE CATEGORIAMODEL]

--- src/main/java/com/drugstoreMariaDasDores/drugstore/model/CategoriaModel.java
+++ src/main/java/com/drugstoreMariaDasDores/drugstore/model/CategoriaModel.java
@@ -1,10 +1,51 @@
 package com.drugstoreMariaDasDores.drugstore.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_categoria")
 public class CategoriaModel {
+	
+	@Id
+	@GeneratedValue
+	private long idCategoria;
+		
+	@NotBlank
+	@Size(min = 3, max = 50)
+	private String tipoProduto;
+	
+	@NotBlank
+	@Size(min = 3, max = 100) 
+	private String descricao;
+
+	public long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getTipoProduto() {
+		return tipoProduto;
+	}
+
+	public void setTipoProduto(String tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
 	
 }
